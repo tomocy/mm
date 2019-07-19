da288781a43eef565d66fbeabdea09558e9a19dd
tomocy <tomocy.dev@gmail.com>
2019-07-19 16:08:34 +0900

    add function to load maze from given fname

--- main.go
+++ main.go
@@ -1,3 +1,25 @@
 package main
 
+import (
+	"bufio"
+	"os"
+)
+
 func main() {}
+
+func loadMaze(name string) ([]string, error) {
+	src, err := os.Open(name)
+	if err != nil {
+		return nil, err
+	}
+	defer src.Close()
+
+	var maze []string
+	scanner := bufio.NewScanner(src)
+	for scanner.Scan() {
+		line := scanner.Text()
+		maze = append(maze, line)
+	}
+
+	return maze, nil
+}
