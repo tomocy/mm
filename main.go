acfdd56c816c215ad2d6a3fe05e2612d26ee4ead
tomocy <tomocy.dev@gmail.com>
2019-07-19 16:11:57 +0900

    modify function to return a maze

--- main.go
+++ main.go
@@ -7,14 +7,14 @@ import (
 
 func main() {}
 
-func loadMaze(name string) ([]string, error) {
+func loadMaze(name string) (maze, error) {
 	src, err := os.Open(name)
 	if err != nil {
 		return nil, err
 	}
 	defer src.Close()
 
-	var maze []string
+	var maze maze
 	scanner := bufio.NewScanner(src)
 	for scanner.Scan() {
 		line := scanner.Text()
