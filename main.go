51042d970b6b66eca27e5758d352cf7f0e9f2dcf
tomocy <tomocy.dev@gmail.com>
2019-07-22 15:24:52 +0900

    add functions to init and clean up

--- main.go
+++ main.go
@@ -4,10 +4,38 @@ import (
 	"bufio"
 	"bytes"
 	"fmt"
+	"log"
 	"os"
+	"os/exec"
 )
 
+func init() {
+	if err := activateCBTerm(); err != nil {
+		log.Fatalf("failed to activate cbreak terminal: %s\n", err)
+	}
+}
+
+func activateCBTerm() error {
+	cmd := exec.Command("/bin/stty", "cbreak", "-echo")
+	cmd.Stdin = os.Stdin
+
+	return cmd.Run()
+}
+
 func main() {}
+
+func cleanUp() {
+	if err := activateCookedTerm(); err != nil {
+		log.Fatalf("failed to activate cooked termainal: %s\n", err)
+	}
+}
+
+func activateCookedTerm() error {
+	cmd := exec.Command("/bin/stty", "-cbreak", "echo")
+	cmd.Stdin = os.Stdin
+
+	return cmd.Run()
+}
 
 func loadMaze(name string) (maze, error) {
 	src, err := os.Open(name)
